e2b13bf647e41b2ac5c59dfb20c68f5a947b4b51
gangintheremark <nkh9587@naver.com>
2021-11-10 14:25:34 +0900

    pull and revise

--- project01/1-5.cpp
+++ project01/1-5.cpp
@@ -4,40 +4,54 @@
 
 using namespace cv;
 using namespace std;
-// 히스토그램 평활화 
-void main() {
+// 히스토그램 평활화
+void main()
+{
 
 	Mat image = imread("flower.png", IMREAD_GRAYSCALE);
 	Mat result;
 
-	int histo[256] = { 0, };
- 
-	for (int i = 0; i < image.rows; i++) {  // 히스토그램 생성
-		for (int j = 0; j < image.cols; j++) {
-			int value = image.at<uchar>(i, j); 
+	int histo[256] = {
+		0,
+	};
+
+	for (int i = 0; i < image.rows; i++)
+	{ // 히스토그램 생성
+		for (int j = 0; j < image.cols; j++)
+		{
+			int value = image.at<uchar>(i, j);
 			histo[value] += 1;
 		}
 	}
 
-	int cumulative_histo[256] = { 0, };
+	int cumulative_histo[256] = {
+		0,
+	};
 	int sum = 0;
 
-	for (int i = 1; i < 256; i++) { // 누적 히스토그램 
+	for (int i = 1; i < 256; i++)
+	{ // 누적 히스토그램
 		sum += histo[i];
 		cumulative_histo[i] = sum;
 	}
 
-	float normalized_cumulative_histo[256] = { 0.0, };  // 정규화된 누적 히스토그램 
+	float normalized_cumulative_histo[256] = {
+		0.0,
+	}; // 정규화된 누적 히스토그램
 	int image_size = image.rows * image.cols;
 
 	for (int i = 0; i < 256; i++)
 		normalized_cumulative_histo[i] = cumulative_histo[i] / (float)image_size;
- 
+
 	result = Mat(image.rows, image.cols, CV_8UC1);
-	int result_histo[256] = { 0, };
-	
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
+	int result_histo[256] = {
+		0,
+	};
+
+	for (int i = 0; i < image.rows; i++)
+	{
+		for (int j = 0; j < image.cols; j++)
+		{
 			result.at<uchar>(i, j) = normalized_cumulative_histo[image.at<uchar>(i, j)] * 255;
 			result_histo[result.at<uchar>(i, j)] += 1;
 		}
--- project03/3-1.cpp
+++ project03/3-1.cpp
@@ -6,18 +6,23 @@ using namespace cv;
 using namespace std;
 
 // 유사연산자 기법
-void main() {
+void main()
+{
 	Mat image = imread("flower.png", IMREAD_GRAYSCALE);
 	Mat result = Mat(image.size(), CV_8U, Scalar(0));
 
 	int mask_size = 3;
 	Point m(mask_size / 2, mask_size / 2);
 
-	for (int i = m.y; i < image.rows - m.y; i++) {  // 입력 행렬 반복 
-		for (int j = m.x; j < image.cols - m.x; j++) {
+	for (int i = m.y; i < image.rows - m.y; i++)
+	{ // 입력 행렬 반복
+		for (int j = m.x; j < image.cols - m.x; j++)
+		{
 			float max = 0;
-			for (int u = 0; u < mask_size; u++) {  // 마스크 원소 순회 
-				for (int v = 0; v < mask_size; v++) { 
+			for (int u = 0; u < mask_size; u++)
+			{ // 마스크 원소 순회
+				for (int v = 0; v < mask_size; v++)
+				{
 					int y = i + u - m.y;
 					int x = j + v - m.x;
 
